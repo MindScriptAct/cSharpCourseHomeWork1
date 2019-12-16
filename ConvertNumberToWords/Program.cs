3a00ba65d77e106c553c0237c63f096d1faf88dc
ksistof1986 <ksistof1986@gmail.com>
2019-12-16 18:53:13 +0200

    First attempts

--- ConvertNumberToWords/Program.cs
+++ ConvertNumberToWords/Program.cs
@@ -17,20 +17,108 @@ namespace ConvertNumberToWords
             Console.WriteLine("Sveiki!");
 
             string numberInput = "";
+            string numberText = "";
 
-            while(numberInput != " ")
+            while (numberInput != " ")
             {
                 Console.WriteLine($"Iveskite skaiciu nuo {FROM_NUMBER} iki {TO_NUMBER}.   (Iveskite Space programai uzbaigti.)");
                 numberInput = Console.ReadLine();
 
-
+               
                 // TODO :au jusu programa!
-
-
+                Console.WriteLine(ChangeNumberToText(numberInput, FROM_NUMBER, TO_NUMBER));
 
             }
+            
             Console.WriteLine("Viso gero!");
             Console.ReadKey();
+        }
+        static string ChangeNumberToText(string numberInput, int FROM_NUMBER, int TO_NUMBER)
+        {
+            int testNumber;
+            string numberText = "";
+
+            //testNumber = Convert.ToInt32(numberText);
+
+            if (CheckIfGoodNumber(numberInput, FROM_NUMBER, TO_NUMBER))
+            {
+                //change number to text
+            }
+            else
+            {
+                //Console.WriteLine(numberInput + " is incorrect format");
+            }
+            //bool goodNumber = CheckIfGoodNumber(testNumber, FROM_NUMBER, TO_NUMBER );
+
+            /*
+            if (numberInput == "-")
+                numberText = "Minus";
+
+            if (numberInput == "0")
+                numberText = "Nulis";
+
+            else if (numberInput == "1")
+                numberText = "Vienas";
+
+            else if(numberInput == "2")
+                numberText = "Du";
+
+            else if(numberInput == "3")
+                numberText = "Trys";
+
+            else if(numberInput == "4")
+                numberText = "Keturi";
+
+            else if(numberInput == "5")
+                numberText = "Penki";
+
+            else if(numberInput == "6")
+                numberText = "Sesi";
+
+            else if(numberInput == "7")
+                numberText = "Septyni";
+
+            else if(numberInput == "8")
+                numberText = "Astuoni";
+
+            else if(numberInput == "9")
+                numberText = "Devyni";
+            */
+            return numberText;
+        }
+
+        static bool CheckIfGoodNumber(string numberInput, int FROM_NUMBER, int TO_NUMBER)
+        {
+           
+            bool goodNumber = false;
+            if (numberInput.All(char.IsDigit))
+            {
+                goodNumber = true;
+                int testNumber = Convert.ToInt32(numberInput);
+                bool numberInRange = CheckIfNumberInRange(testNumber, FROM_NUMBER, TO_NUMBER);
+
+                if (numberInRange)
+                {
+                    Console.WriteLine(" number in range " + numberInRange);
+                }
+                //Console.WriteLine("good number " + numberInput);
+            }
+            else
+                goodNumber = false;
+            //Console.WriteLine("Bad input " + numberInput);
+
+            return goodNumber;
+            
+        }
+
+        static bool CheckIfNumberInRange(int testNumber, int FROM_NUMBER, int TO_NUMBER)
+        {
+            bool numberInRange = false;
+            if (testNumber>= FROM_NUMBER && testNumber <= TO_NUMBER)
+            {
+                numberInRange = true;
+            }
+            return numberInRange;
         }
 
         // bendra funkcija apjungti visom funkcijom kurias jus sukursit.
@@ -45,7 +133,7 @@ namespace ConvertNumberToWords
         // idejos kitom funkcijoms
         //ChangeOnesToText(), ChangeTeensToText, ChangeTensToText, ChangeHundredsToText, ChangeThousandsToText, ChangeMillionsToText, ChangeBilllionsToText
 
-        
+
         //Skaiciai zodziais:  
         // "minus"; 
         // "nulis", "vienas", "du", "trys", "keturi", "penki", "sesi", "septyni", "astuoni", "devyni"; 
