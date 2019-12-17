87f4a8cc7d76bacca45a533c342011f90e58b0ba
ksistof1986 <ksistof1986@gmail.com>
2019-12-17 14:56:01 +0200

    Initial number to text converting concept

--- ConvertNumberToWords/Program.cs
+++ ConvertNumberToWords/Program.cs
@@ -14,6 +14,12 @@ namespace ConvertNumberToWords
             const int FROM_NUMBER = -9;
             const int TO_NUMBER = 9;
 
+            const int TEENS_FROM = -19;
+            const int TEENS_TO = 19;
+
+            const int TENS_FROM = -90;
+            const int TENS_TO = 90;
+
             Console.WriteLine("Sveiki!");
 
             string numberInput = "";
@@ -24,91 +30,93 @@ namespace ConvertNumberToWords
                 Console.WriteLine($"Iveskite skaiciu nuo {FROM_NUMBER} iki {TO_NUMBER}.   (Iveskite Space programai uzbaigti.)");
                 numberInput = Console.ReadLine();
 
-               
                 // TODO :au jusu programa!
-                Console.WriteLine(ChangeNumberToText(numberInput, FROM_NUMBER, TO_NUMBER));
+                Console.WriteLine(ChangeNumberToText(numberInput, FROM_NUMBER, TO_NUMBER, TEENS_FROM, TEENS_TO, TENS_FROM, TENS_TO));
 
             }
             
             Console.WriteLine("Viso gero!");
             Console.ReadKey();
         }
-        static string ChangeNumberToText(string numberInput, int FROM_NUMBER, int TO_NUMBER)
+        static string ChangeNumberToText(string numberInput, int FROM_NUMBER, int TO_NUMBER, int TEENS_FROM, int TEENS_TO, int TENS_FROM, int TENS_TO)
         {
             int testNumber;
             string numberText = "";
-
+            string resultFormat;
+            string resultInRange;
             //testNumber = Convert.ToInt32(numberText);
 
             if (CheckIfGoodNumber(numberInput, FROM_NUMBER, TO_NUMBER))
             {
-                //change number to text
+                testNumber = Convert.ToInt32(numberInput);
+                resultFormat = "Correct format";
+           
+                if (CheckIfNumberInRange(testNumber, FROM_NUMBER, TO_NUMBER))
+                {
+                    resultInRange = "Number in range ";
+                }
+                else
+                    resultInRange = "Number not in range ";
+
+                Console.WriteLine("Range result:"+ numberInput + " " + resultInRange + " " + CheckIfNumberInRange(testNumber, FROM_NUMBER, TO_NUMBER));
+                  
+
+                Console.WriteLine("Text: ");
+                //ones
+                if (testNumber>= FROM_NUMBER && testNumber <= TO_NUMBER)
+                {
+                    Console.WriteLine(testNumber + " " + ChangeOnesToText(testNumber, FROM_NUMBER, TO_NUMBER));
+                }
+
+                //teens
+                else if ((testNumber >= TEENS_FROM && testNumber < FROM_NUMBER) || (testNumber > TO_NUMBER && testNumber <= TEENS_TO))
+                {             
+                    Console.WriteLine(testNumber + " " + ChangeTeensToText(testNumber, TEENS_FROM, TEENS_TO));
+                }
+
+                //tens
+                else if ((testNumber >= TENS_FROM && testNumber < TEENS_FROM) || (testNumber > TEENS_TO && testNumber <= TENS_TO))
+                {
+                    //35
+                    Console.WriteLine(testNumber + " " + ChangeTensToText(testNumber, TENS_FROM, TENS_TO));
+                }
+                
             }
+             
             else
-            {
-                //Console.WriteLine(numberInput + " is incorrect format");
-            }
-            //bool goodNumber = CheckIfGoodNumber(testNumber, FROM_NUMBER, TO_NUMBER );
-
-            /*
-            if (numberInput == "-")
-                numberText = "Minus";
-
-            if (numberInput == "0")
-                numberText = "Nulis";
-
-            else if (numberInput == "1")
-                numberText = "Vienas";
-
-            else if(numberInput == "2")
-                numberText = "Du";
-
-            else if(numberInput == "3")
-                numberText = "Trys";
-
-            else if(numberInput == "4")
-                numberText = "Keturi";
-
-            else if(numberInput == "5")
-                numberText = "Penki";
-
-            else if(numberInput == "6")
-                numberText = "Sesi";
-
-            else if(numberInput == "7")
-                numberText = "Septyni";
-
-            else if(numberInput == "8")
-                numberText = "Astuoni";
-
-            else if(numberInput == "9")
-                numberText = "Devyni";
-            */
+                resultFormat = "Incorrect format";
+            
+            Console.WriteLine("Format result "+ numberInput + " " + resultFormat + " " + CheckIfGoodNumber(numberInput, FROM_NUMBER, TO_NUMBER));
             return numberText;
         }
 
         static bool CheckIfGoodNumber(string numberInput, int FROM_NUMBER, int TO_NUMBER)
         {
-           
+            /*
             bool goodNumber = false;
-            if (numberInput.All(char.IsDigit))
+            for (int i = FROM_NUMBER; i <= TO_NUMBER; i++)
             {
-                goodNumber = true;
-                int testNumber = Convert.ToInt32(numberInput);
-                bool numberInRange = CheckIfNumberInRange(testNumber, FROM_NUMBER, TO_NUMBER);
 
-                if (numberInRange)
+                if(Convert.ToString(i) == numberInput)
                 {
-                    Console.WriteLine(" number in range " + numberInRange);
+                    goodNumber = true;
                 }
-                //Console.WriteLine("good number " + numberInput);
-            }
-            else
-                goodNumber = false;
-            //Console.WriteLine("Bad input " + numberInput);
 
+            }
             return goodNumber;
-            
+            */
+            /*
+             if (numberInput.All(char.IsDigit))
+                 goodNumber = true;
+             else
+                 goodNumber = false;
+
+             return goodNumber;
+             */
+
+            int i = 0;
+            bool result = int.TryParse(numberInput, out i);
+            return result;
         }
 
         static bool CheckIfNumberInRange(int testNumber, int FROM_NUMBER, int TO_NUMBER)
@@ -117,18 +125,111 @@ namespace ConvertNumberToWords
             if (testNumber>= FROM_NUMBER && testNumber <= TO_NUMBER)
             {
                 numberInRange = true;
+
             }
             return numberInRange;
         }
 
+        static string ChangeOnesToText(int testNumber, int FROM_NUMBER, int TO_NUMBER)
+        {
+            string ones = "";
+            if (testNumber == 0)
+                ones = "nulis";
+
+            else if (testNumber != 0 && testNumber <= TO_NUMBER && testNumber>= FROM_NUMBER)
+            {
+                if (testNumber == 1 || testNumber == -1)
+                ones = "Vienas";
+                else if (testNumber == 2 || testNumber == -2)
+                ones = "Du";
+                else if (testNumber == 3 || testNumber == -3)
+                ones = "Trys";
+                else if (testNumber == 4 || testNumber == -4)
+                 ones = "Keturi";
+                else if (testNumber == 5 || testNumber == -5)
+                    ones = "Penki";
+                else if (testNumber == 6 || testNumber == -6)
+                    ones = "Sesi";
+                else if (testNumber == 7 || testNumber == -7)
+                    ones = "Septyni";
+                else if (testNumber == 8 || testNumber == -8)
+                    ones = "Astuoni";
+                else if (testNumber == 9 || testNumber == -9)
+                 ones = "Devyni";
+
+                if (testNumber < 0)
+                {
+                    ones = "Minus "+ ones;
+                }
+            }
+            return ones;    
+        }
+
+        static string ChangeTeensToText(int testNumber, int TEENS_FROM, int TEENS_TO)
+        {
+            string teens = "";
+                if (testNumber == 10 || testNumber == -10)
+                    teens = "Vienuolika";
+                else if (testNumber == 11 || testNumber == -11)
+                    teens = "Vienuolika";
+                else if (testNumber == 12 || testNumber == -12)
+                    teens = "Dvylika";
+                else if (testNumber == 13 || testNumber == -13)
+                    teens = "Trylika";
+                else if (testNumber == 14 || testNumber == -14)
+                    teens = "Keturiolika";
+                else if (testNumber == 15 || testNumber == -15)
+                    teens = "Penkiolika";
+                else if (testNumber == 16 || testNumber == -16)
+                    teens = "Sesiolika";
+                else if (testNumber == 17 || testNumber == -17)
+                    teens = "Septyniolika";
+                else if (testNumber == 18 || testNumber == -18)
+                    teens = "Astuoniolika";
+                else if (testNumber == 19 || testNumber == -19)
+                    teens = "Devyniolika";
+                if (testNumber < 0)
+                {
+                    teens = "Minus " + teens;
+                }
+            
+            return teens;
+        }
+        static string ChangeTensToText(int testNumber, int TENS_FROM, int TENS_TO)
+        {
+            string tens = "";
+
+            if (testNumber >= 20 && testNumber <=29 || testNumber >= -29 && testNumber <= -20)
+                tens = "Dvidesimt";
+            else if (testNumber == 30 || testNumber == -30)
+                tens = "Trisdesimt";
+            else if (testNumber == 40 || testNumber == -40)
+                tens = "Keturiasdesimt";
+            else if (testNumber == 50 || testNumber == -50)
+                tens = "Penkiasdesimt";
+            else if (testNumber == 60 || testNumber == -60)
+                tens = "Sesiolika";
+            else if (testNumber == 70 || testNumber == -70)
+                tens = "Septyniasdesimt";
+            else if (testNumber == 80 || testNumber == -80)
+                tens = "Astuoniasdesimt";
+            else if (testNumber == 90 || testNumber == -90)
+                tens = "Devyniasdesimt";
+            if (testNumber < 0)
+            {
+                tens = "Minus " + tens;
+            }
+
+            return tens;
+        }
         // bendra funkcija apjungti visom funkcijom kurias jus sukursit.
-        // ... ChangeNumberToText()
+        // ... ChangeNumberToText()+
 
         // funkcija gauna string skaiciu, patikrina ar skaicius teisingu formatu. Pvz: "123", "-123" grazina true. "12a3", "1-23" grazina false.
-        // ... CheckIfGoodNumber(...)
+        // ... CheckIfGoodNumber(...)+
 
         // funkcija grazina true jei skaicius checkNumber yar tarp fromNumber ir toNumber (imtinai)
-        // ... CheckIfNumberInRange(...)
+        // ... CheckIfNumberInRange(...)+
 
         // idejos kitom funkcijoms
         //ChangeOnesToText(), ChangeTeensToText, ChangeTensToText, ChangeHundredsToText, ChangeThousandsToText, ChangeMillionsToText, ChangeBilllionsToText
