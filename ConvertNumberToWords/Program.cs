40ec8e6258b26a3d1e7a866d8a475ff5162cd150
ksistof1986 <ksistof1986@gmail.com>
2019-12-17 16:24:08 +0200

    thousands in progress

--- ConvertNumberToWords/Program.cs
+++ ConvertNumberToWords/Program.cs
@@ -17,8 +17,14 @@ namespace ConvertNumberToWords
             const int TEENS_FROM = -19;
             const int TEENS_TO = 19;
 
-            const int TENS_FROM = -90;
-            const int TENS_TO = 90;
+            const int TENS_FROM = -99;
+            const int TENS_TO = 99;
+
+            const int HUNDREDS_FROM = -999;
+            const int HUNDREDS_TO = 999;
+
+            const int THOUSANDS_FROM = -100000;
+            const int THOUSANDS_TO = 100000;
 
             Console.WriteLine("Sveiki!");
 
@@ -31,14 +37,14 @@ namespace ConvertNumberToWords
                 numberInput = Console.ReadLine();
 
                 // TODO :au jusu programa!
-                Console.WriteLine(ChangeNumberToText(numberInput, FROM_NUMBER, TO_NUMBER, TEENS_FROM, TEENS_TO, TENS_FROM, TENS_TO));
+                Console.WriteLine(ChangeNumberToText(numberInput, FROM_NUMBER, TO_NUMBER, TEENS_FROM, TEENS_TO, TENS_FROM, TENS_TO, HUNDREDS_FROM, HUNDREDS_TO, THOUSANDS_FROM, THOUSANDS_TO));
 
             }
             
             Console.WriteLine("Viso gero!");
             Console.ReadKey();
         }
-        static string ChangeNumberToText(string numberInput, int FROM_NUMBER, int TO_NUMBER, int TEENS_FROM, int TEENS_TO, int TENS_FROM, int TENS_TO)
+        static string ChangeNumberToText(string numberInput, int FROM_NUMBER, int TO_NUMBER, int TEENS_FROM, int TEENS_TO, int TENS_FROM, int TENS_TO, int HUNDREDS_FROM, int HUNDREDS_TO ,int THOUSANDS_FROM, int THOUSANDS_TO)
         {
             int testNumber;
             string numberText = "";
@@ -58,29 +64,38 @@ namespace ConvertNumberToWords
                 else
                     resultInRange = "Number not in range ";
 
-                Console.WriteLine("Range result:"+ numberInput + " " + resultInRange + " " + CheckIfNumberInRange(testNumber, FROM_NUMBER, TO_NUMBER));
-                  
+                Console.WriteLine("Range result:"+ numberInput + " " + resultInRange + " " + CheckIfNumberInRange(testNumber, FROM_NUMBER, TO_NUMBER));                 
 
                 Console.WriteLine("Text: ");
                 //ones
                 if (testNumber>= FROM_NUMBER && testNumber <= TO_NUMBER)
                 {
-                    Console.WriteLine(testNumber + " " + ChangeOnesToText(testNumber, FROM_NUMBER, TO_NUMBER));
+                    Console.WriteLine(testNumber + " " + CheckIfMinus(testNumber)+" "+ChangeOnesToText(testNumber, FROM_NUMBER, TO_NUMBER));
                 }
 
                 //teens
                 else if ((testNumber >= TEENS_FROM && testNumber < FROM_NUMBER) || (testNumber > TO_NUMBER && testNumber <= TEENS_TO))
                 {             
-                    Console.WriteLine(testNumber + " " + ChangeTeensToText(testNumber, TEENS_FROM, TEENS_TO));
+                    Console.WriteLine(testNumber + " " + CheckIfMinus(testNumber) + " " + ChangeTeensToText(testNumber, TEENS_FROM, TEENS_TO));
                 }
 
                 //tens
                 else if ((testNumber >= TENS_FROM && testNumber < TEENS_FROM) || (testNumber > TEENS_TO && testNumber <= TENS_TO))
                 {
-                    //35
-                    Console.WriteLine(testNumber + " " + ChangeTensToText(testNumber, TENS_FROM, TENS_TO));
+                    Console.WriteLine(testNumber + " " + CheckIfMinus(testNumber) + " " + ChangeTensToText(testNumber, FROM_NUMBER, TO_NUMBER));
                 }
-                
+                //hundreds
+                else if ((testNumber >= HUNDREDS_FROM && testNumber < TENS_FROM) || (testNumber > TENS_TO && testNumber <= HUNDREDS_TO))
+                {
+                    Console.WriteLine(testNumber + " " + CheckIfMinus(testNumber) + " " + ChangeHundredsToText(testNumber, FROM_NUMBER, TO_NUMBER, TEENS_FROM, TEENS_TO, HUNDREDS_FROM, HUNDREDS_TO));
+                }
+                //thousands
+                else if ((testNumber >= THOUSANDS_FROM && testNumber < HUNDREDS_FROM) || (testNumber > HUNDREDS_TO && testNumber <= THOUSANDS_TO))
+                {
+                    Console.WriteLine("TUKSTANTINIAI  ");
+                    Console.WriteLine(testNumber + " " + CheckIfMinus(testNumber) + " " + ChangeThousandsToText(testNumber, FROM_NUMBER, TO_NUMBER, TEENS_FROM, TEENS_TO, TENS_FROM, TENS_TO,HUNDREDS_FROM,HUNDREDS_TO));
+                }
+
             }
              
             else
@@ -130,6 +145,14 @@ namespace ConvertNumberToWords
             return numberInRange;
         }
 
+        static string CheckIfMinus(int testNumber)
+        {
+            string minus = "";
+            if (testNumber < 0)
+                minus = "Minus";
+
+            return minus;
+        }
         static string ChangeOnesToText(int testNumber, int FROM_NUMBER, int TO_NUMBER)
         {
             string ones = "";
@@ -156,11 +179,12 @@ namespace ConvertNumberToWords
                     ones = "Astuoni";
                 else if (testNumber == 9 || testNumber == -9)
                  ones = "Devyni";
-
+                /*
                 if (testNumber < 0)
                 {
                     ones = "Minus "+ ones;
                 }
+                */
             }
             return ones;    
         }
@@ -169,7 +193,7 @@ namespace ConvertNumberToWords
         {
             string teens = "";
                 if (testNumber == 10 || testNumber == -10)
-                    teens = "Vienuolika";
+                    teens = "Desimt";
                 else if (testNumber == 11 || testNumber == -11)
                     teens = "Vienuolika";
                 else if (testNumber == 12 || testNumber == -12)
@@ -188,39 +212,132 @@ namespace ConvertNumberToWords
                     teens = "Astuoniolika";
                 else if (testNumber == 19 || testNumber == -19)
                     teens = "Devyniolika";
-                if (testNumber < 0)
+            /*
+            if (testNumber < 0)
                 {
                     teens = "Minus " + teens;
                 }
-            
+            */
             return teens;
+
         }
-        static string ChangeTensToText(int testNumber, int TENS_FROM, int TENS_TO)
+        static string ChangeTensToText(int testNumber, int FROM_NUMBER, int TO_NUMBER)
         {
             string tens = "";
+            int ones = testNumber % 10;
 
             if (testNumber >= 20 && testNumber <=29 || testNumber >= -29 && testNumber <= -20)
                 tens = "Dvidesimt";
-            else if (testNumber == 30 || testNumber == -30)
+            else if (testNumber >= 30 && testNumber <= 39 || testNumber >= -39 && testNumber <= -30)
                 tens = "Trisdesimt";
-            else if (testNumber == 40 || testNumber == -40)
+            else if (testNumber >= 40 && testNumber <= 49 || testNumber >= -49 && testNumber <= -40)
                 tens = "Keturiasdesimt";
-            else if (testNumber == 50 || testNumber == -50)
+            else if (testNumber >= 50 && testNumber <= 59 || testNumber >= -59 && testNumber <= -50)
                 tens = "Penkiasdesimt";
-            else if (testNumber == 60 || testNumber == -60)
-                tens = "Sesiolika";
-            else if (testNumber == 70 || testNumber == -70)
+            else if (testNumber >= 60 && testNumber <= 69 || testNumber >= -69 && testNumber <= -60)
+                tens = "Sesiasdesimt";
+            else if (testNumber >= 70 && testNumber <= 79 || testNumber >= -79 && testNumber <= -70)
                 tens = "Septyniasdesimt";
-            else if (testNumber == 80 || testNumber == -80)
+            else if (testNumber >= 80 && testNumber <= 89 || testNumber >= -89 && testNumber <= -80)
                 tens = "Astuoniasdesimt";
-            else if (testNumber == 90 || testNumber == -90)
+            else if (testNumber >= 90 && testNumber <= 99 || testNumber >= -99 && testNumber <= -90)
                 tens = "Devyniasdesimt";
-            if (testNumber < 0)
+
+            if (ones != 0)
             {
-                tens = "Minus " + tens;
+                tens= tens +" "+ ChangeOnesToText(ones, FROM_NUMBER, TO_NUMBER);
             }
 
             return tens;
+        }
+        static string ChangeHundredsToText(int testNumber, int FROM_NUMBER, int TO_NUMBER, int TEENS_FROM, int TEENS_TO, int HUNDREDS_FROM, int HUNDREDS_TO)
+        {
+            string hundreds = "";
+            int tens = testNumber % 100;
+            int teens = testNumber % 10;
+
+            if (testNumber >= 100 && testNumber <= 199 || testNumber >= -199 && testNumber <= -100)
+                hundreds = "Simtas";
+
+            int div = testNumber / 100;
+            if (div < 0)  div = div * -1; 
+            for (int i=1; i<=9; i++)
+            {
+                if (div == i)
+                {
+                    hundreds = ChangeOnesToText(div, i, 9) + " Simtai";
+                }
+            }
+           
+            if (tens != 0)
+            {
+                if ((tens >= TEENS_FROM && tens < FROM_NUMBER) || (tens > TO_NUMBER && tens <= TEENS_TO))
+                {
+                    hundreds = hundreds + " " + ChangeTeensToText(tens, TEENS_FROM, TEENS_TO);
+                }
+                else
+                hundreds = hundreds + " " + ChangeTensToText(tens, FROM_NUMBER, TO_NUMBER);
+            }
+
+            return hundreds;
+        }
+
+        static string ChangeThousandsToText(int testNumber, int FROM_NUMBER, int TO_NUMBER, int TEENS_FROM, int TEENS_TO, int TENS_FROM, int TENS_TO, int HUNDREDS_FROM, int HUNDREDS_TO)
+        {
+            string thousands = "";
+            int hundreds = testNumber % 1000;
+
+            if (testNumber >= 1000 && testNumber <= 1999 || testNumber >= -1999 && testNumber <= -1000)
+                thousands = "Tukstantis";
+
+            int div = testNumber / 1000;
+            if (div < 0) div = div * -1;
+            for (int i = 1; i <= 9; i++)
+            {
+                if (div == i)
+                {
+                    thousands = ChangeOnesToText(div, i, 9) + " Tukstanciai";
+                }
+            }
+
+            for (int i = 10; i <= 99; i++)
+            {
+                if (div == i)
+                {
+                    thousands = ChangeTeensToText(div, 10, 99) + " Tukstanciu";
+                }
+            }
+
+            for (int i = 20; i <= 99; i++)
+            {
+                if (div == i)
+                {
+                    thousands = ChangeTensToText(div, 10, 99) + " Tukstanciu";
+                }
+            }
+            for (int i = 100; i <= 999; i++)
+            {
+                if (div == i)
+                {
+                    thousands = ChangeHundredsToText(div, 1,9, 10, 19, 100, 99) + " Tukstanciu";
+                }
+            }
+
+            if (hundreds != 0)
+            {
+                if ((hundreds >= HUNDREDS_FROM && hundreds < TENS_FROM) || (hundreds > TENS_TO && hundreds <= HUNDREDS_TO))
+                {
+                    thousands = thousands + " " + ChangeHundredsToText(hundreds, FROM_NUMBER, TO_NUMBER, TEENS_FROM, TEENS_TO, HUNDREDS_FROM, HUNDREDS_TO);
+                }
+                else if ((hundreds >= TEENS_FROM && hundreds < FROM_NUMBER) || (hundreds > TO_NUMBER && hundreds <= TEENS_TO))
+                {
+                    thousands = thousands + " " + ChangeTeensToText(hundreds, TEENS_FROM, TEENS_TO);
+                }
+                else
+                    thousands = thousands + " " + ChangeTensToText(hundreds, FROM_NUMBER, TO_NUMBER);
+            }
+
+            return thousands;
         }
         // bendra funkcija apjungti visom funkcijom kurias jus sukursit.
         // ... ChangeNumberToText()+
